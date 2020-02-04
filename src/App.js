3e1e3b2dab5e7d05cc6ef1fa58cde0562375d3f6
Damion Gomez <damiong28@gmail.com>
2020-02-04 13:41:56 -0700

    add complex javascript and convert to arrow functions

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
 import React from 'react';
 import './App.css';
 
+const list = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5, 
+    objectID: 1,
+  },
+];
+
 function App() {
-  var helloWorld = 'Welcome to the Road to Learn React';
+  const helloWorld = 'Welcome to the Road to Learn React';
   let user = {
     first_name: 'Damion',
     last_name: 'Gomez',
@@ -12,6 +31,16 @@ function App() {
     <div className="App">
       <h2>{helloWorld}</h2>
       <h3>Implemented by: {user.first_name} {user.middle}. {user.last_name}</h3>
+      {list.map(item => 
+        <div key={item.objectID}>
+          <span>
+            <a href={item.url}>{item.title} </a>
+          </span>
+          <span>{item.author}</span>
+          <span>{item.num_comments}</span>
+          <span>{item.points}</span>
+        </div>
+      )}
     </div>
   );
 }
